03fca4e810f38b95bc35c6f5b1ddd979c9e198d7
satish.patra <satish.patra@sapper.ai>
2025-06-12 10:00:40 +0530

    slices, functions, closures added

---
+++ multiple-return-values.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func vals() (int, int) {
+	return 3, 7
+}
+
+func main() {
+	a, b := vals()
+	fmt.Println(a)
+	fmt.Println(b)
+
+	_, c := vals()
+	fmt.Println(c)
+
+	d, _ := vals()
+	fmt.Println(d)
+
+	fmt.Println(vals())
+}
