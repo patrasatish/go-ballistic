f618dab13cfa08daa08d380b91984509e887bdac
satish.patra <psatishkumar720@gmail.com>
2025-06-14 23:05:10 +0530

    Update interfaces.go

--- interfaces.go
+++ interfaces.go
@@ -18,6 +18,7 @@ type circle struct {
 	radius float64
 }
 
+// to implement an interface in Go, just implement all methods in the interface
 func (r rect) area() float64 {
 	return r.height * r.width
 }
@@ -34,12 +35,14 @@ func (c circle) perim() float64 {
 	return 2 * math.Pi * c.radius
 }
 
+// can call all methods in the named interface - geometry by all implementors
 func measure(g geometry) {
 	fmt.Println(g)
 	fmt.Println(g.area())
 	fmt.Println(g.perim())
 }
 
+// can detect the runtime type of an interface value
 func detectCircle(g geometry) {
 	if c, ok := g.(circle); ok {
 		fmt.Println("circle with radius", c.radius)
@@ -52,6 +55,8 @@ func main() {
 	r := rect{width: 3, height: 4}
 	c := circle{radius: 5}
 
+	// rect, circle both implement geometry, so can use instances of these structs
+	// as arguments to measure
 	measure(r)
 	measure(c)
 
